2b07cdcac32d9bf3bcf1fdfdad9539654d93d911
Farhan Hanif <madjavacoder@gmail.com>
2023-07-10 20:44:37 +0700

    added jsketch

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: process.env.EVENT_NAME });
 });
 
+router.get('/signs', function(req, res, next) {
+  res.render('add_signs', { title: process.env.EVENT_NAME, scripts: [{script: "/javascripts/jsketch.min.js"}] });
+});
+
 module.exports = router;
